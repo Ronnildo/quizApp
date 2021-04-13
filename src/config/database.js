310a66cf3757deb85c2fed193fa9a401fc7336c0
Ronnildo <ronildosp03@gmail.com>
2021-04-13 20:49:51 -0300

    migrations

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     host: "localhost",
     username: "postgres",
     password: "123456",
-    database: "appQuiz",
+    database: "postgres",
     define: {
       timestamps: true,
       underscored: true,
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-    return res.json({mensagem: "QuizApp"});
-})
+  return res.json({ mensagem: "QuizApp" });
+});
 
 export default routes;
