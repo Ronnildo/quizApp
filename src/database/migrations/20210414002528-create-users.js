290df5e96656bf2e894dc88ee31a3fbd366e2c62
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2021-04-13 22:33:10 -0300

    configuraçoes

---
+++ src/database/migrations/20210414002528-create-users.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const { password } = require("../../config/database");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("users");
+  },
+};
