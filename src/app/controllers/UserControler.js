f4663656d90e742fed861423e8b2cc79abeacedc
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2021-04-16 21:49:09 -0300

    controller add

---
+++ src/app/controllers/UserControler.js
@@ -0,0 +1,13 @@
+import User from "../models/User";
+
+class userController {
+    async store(req, res) {
+        const { id, nome, email, senha } = await User.create(
+            req.body
+        );
+
+        return res.json({ id, nome, email, senha });
+    }
+}
+
+export default new userController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import User from './app/models/User'
 const routes = new Router();
 
 routes.get("/", (req, res) => {
     return res.json({mensagem: "QuizApp"});
+})
+routes.get("/user", async (req, res) => {
+    const user = await User.create({
+        nome: "Eduardo",
+        email: "eduardo@gmail.com",
+        senha: "123456",
+    });
+    res.json(user);
 })
 
 export default routes;
