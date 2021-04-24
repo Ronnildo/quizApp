053b7ffa060c7cc58325d271d9dc47fe1ff2b21d
Ronnildo <ronildosp03@gmail.com>
2021-04-24 11:29:17 -0300

    add Contoller de Usuarios e controller de sessao

--- src/quiz/controller/UserController.js
+++ src/quiz/controller/UserController.js
@@ -1,13 +1,36 @@
 import User from "../models/User";
+import { password } from '../../config/database'
+import * as Yup from 'yup';
 
 class userController {
     async store(req, res) {
+
+        const esquema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            senha: Yup.string().required().min(6),
+        });
+        
+        if(!(await(esquema.isValid(req.body)))){
+            return res.status(400).json({mensagem: "Campos Invalidos!"})
+        }
+
+        const userExists = await User.findOne({where: {email: req.body.email } });
+
+        if(userExists){
+            return res.status(400).json({mensagem: "Usuario já existe"});
+        }
+
         const { id, name, email, senha } = await User.create(
             req.body
         );
-        console.log(id);
+
         return res.json({ id, name, email, senha });
     }
+
+
+
+    
 }
 
 export default new userController();
